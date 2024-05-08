c53c9ba4446c64616b6b56cc2e43b949f2374c48
unknown <florezf90@gmail.com>
2024-05-08 12:14:08 -0500

    finished header static component with the desired design and also fixed the compatibility issues with react-bootstrap library, just have to re learn the concept of server side rendering and client side rendering

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,45 @@
-import { Stack } from "react-bootstrap";
-import { NavItem } from "react-bootstrap";
-import Link from "next/link";
-import NavigationBtn from "./navigationbtn";
-export default function Navbar() {
+'use client';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from './global.module.css'
+import { CardTitle } from "react-bootstrap";
+
+function Header() {
   return (
-    <Stack direction="horizontal" gap={3} className="p-3 bg-dark  ">
-      <NavItem>
-        <Link href="/">
-          <NavigationBtn name="Home" />
-        </Link>
-      </NavItem>
-      <NavItem>
-        <Link href="/about">
-          <NavigationBtn name="About" />
-        </Link>
-      </NavItem>
-      <NavItem>
-        <Link href="/services">
-          <NavigationBtn name="services" />
-        </Link>
-      </NavItem>
-      <NavItem>
-        <Link href="/contact">
-          <NavigationBtn name="contact" />
-        </Link>
-      </NavItem>
-      
-    </Stack>
+    <>
+      <Navbar className={styles.divisor}>
+        <Nav className="me-auto">
+          <CardTitle className={styles.brand}>Florezf90 Gym</CardTitle>
+        </Nav>
+        <Nav className="me-auto">
+          <Nav.Link className={styles.number}>1-800-555-1234</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link className={styles.number} >
+            Mon - Fri: 8:00AM - 7:00PM | Sat - Sun: Closed
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="my-3 mx-4" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="my-3 mx-4" >
+            <Nav className="me-auto">
+             <Nav.Link href="/" >Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/services">Services</Nav.Link>
+              <Nav.Link href="/contact">
+                Get in Touch
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
+
+export default Header;
