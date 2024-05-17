089a0650a552993f89afa4c527a9dee611352f2e
unknown <florezf90@gmail.com>
2024-05-16 23:08:15 -0500

    edited styling in the existing components

--- src/components/fixedBacgroundSec.jsx
+++ src/components/fixedBacgroundSec.jsx
@@ -7,11 +7,11 @@ import styles from './global.module.css';
 export default function FixedBackgroundSec({ title, subtitle, cards }) {
     return (
       <div className={styles.fixedImgContainer}>
-        <header className={`${styles.header} mx-auto`}>
-          <h1 className="fs-1 text-center mb-3 font-weight-bold">{title}</h1>
+        <header className={`${styles.header} mx-auto `}>
+          <h1 className="fs-1 text-center mb-3 ">{title}</h1>
         </header>
-        <p className="fs-3 text-center my-3 text-light ">{subtitle}</p>
-        <Container className="d-flex   flex-column flex-lg-row my-5    justify-content-between">
+        <p className="fs-3 text-center my-2 text-white  shadow   ">{subtitle}</p>
+        <Container className={`${styles.cards} d-flex  flex-column flex-lg-row justify-content-between`}>
           {cards}
         </Container>
       </div>
