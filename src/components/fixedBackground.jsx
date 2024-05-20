deb4fd2fe6fa06dd7e8a524046e0bdfdb5c48b43
unknown <florezf90@gmail.com>
2024-05-19 22:36:40 -0500

    trying to edit text to all screen sizes

--- src/components/fixedBackground.jsx
+++ src/components/fixedBackground.jsx
@@ -1,15 +1,15 @@
 import styles from './components.module.css';
 
-export default function FixedBackground ({background}) {
+export default function FixedBackground ({background, title, text }) {
 
     return (
       <div
         className={` d-flex justify-content-start align-items-center  ${styles.fixedImgContainer}`} 
         style={{backgroundImage: `url(${background.src})`}}
       >
-        <div className='w-50 mb-5 ' >
-        <h1 className={`${styles.fixedImgText}`}>Florezf90 Gym</h1>
-        <p className={styles.fixedImgText}>1-800-555-1234</p>
+        <div className='w-40 mb-5 ' >
+        <h1 className={`${styles.fixedImgText} mt-5`}>{title}</h1>
+        <p className={styles.fixedImgText}>{text}</p>
         </div>
       </div>
     );
