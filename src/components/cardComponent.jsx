bf66e0f73f53cc0a9a7ede72fe8cfc250176f0db
unknown <florezf90@gmail.com>
2024-05-29 06:54:34 -0500

    modified the cards component to make it scalable, this was achieved by using props as tName and tPhoto, using an array that will have the data I wanted to pass to the card and then, in the parent component I used a map function that will go over the array and create a new cardComponent element for every index of the array, achieving the end result

--- src/components/cardComponent.jsx
+++ src/components/cardComponent.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { motion } from "framer-motion";
 import styles from "./components.module.css";
+import Link from "next/link";
 
-export default function ImageCard() {
+
+export default function ImageCard({ tName, tPhoto }) {
   const [hover, setHover] = useState(false);
 
   return (
@@ -16,11 +18,7 @@ export default function ImageCard() {
     >
       <Card className={`m-3 ${styles.imageCard}`}>
         <div className={styles.imageWrapper}>
-          <Card.Img
-            variant="top"
-            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className={styles.cardImage}
-          />
+          <Card.Img variant="top" src={tPhoto} className={styles.cardImage} alt={`${tName} trainer`} />
           <div
             className={`${styles.imageOverlay} ${
               hover ? styles.imageOverlayHover : ""
@@ -29,12 +27,14 @@ export default function ImageCard() {
         </div>
         <Card.Body className={styles.cardBody}>
           {hover && (
-            <Card.Title className={styles.cardTitle}>Card Title</Card.Title>
+            <Card.Title className={styles.cardTitle}>{tName}</Card.Title>
           )}
           {hover && (
-            <Button variant="primary" className={styles.cardButton}>
-              Click Me
-            </Button>
+            <Link href={"/about"}>
+              <Button variant="primary" className={styles.cardButton}>
+                Learn More
+              </Button>
+            </Link>
           )}
         </Card.Body>
       </Card>
---
+++ src/utils/trainersInfo.js
@@ -0,0 +1,28 @@
+import saes from "../../public/trainer.webp";
+
+export default function trainersInfo () {
+    const trainers = [
+        {
+            tName: "Trainer 1",
+            tPhoto: saes.src,
+        },
+        {
+            tName: "Trainer 2",
+            tPhoto: saes.src,
+        },
+        {
+            tName: "Trainer 3",
+            tPhoto: saes.src,
+        },
+        {
+            tName: "Trainer 4",
+            tPhoto: saes.src,
+        },
+        {
+            tName: "Trainer 5",
+            tPhoto: saes.src,
+        }
+    ]
+
+    return { trainers };
+}
